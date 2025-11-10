06940654d071da17b9fbf49a89f4d025db3f87f3
AlexMurray1127 <alexmurray1127@gmail.com>
2025-11-10 10:59:31 -0500

    Detect Change
    Modified to detect change from previous frame using absdiff()

--- mirror.py
+++ mirror.py
@@ -7,6 +7,8 @@ cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+
+first = True
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -17,17 +19,36 @@ while True:
         break
     frame = cv.flip(frame, 1)
 
+    # Operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    if first:
+        prev_gray = gray
+        first = False
+    # Calculate change in gray
+    delta = cv.absdiff(gray, prev_gray).sum()
+
     # Display Text
+    cv.putText(frame, str(delta), (50,300),
+                cv.FONT_HERSHEY_SIMPLEX,
+                2,              # font_scale
+                (255,255,255),    # color
+                4)              # thickness
+
     cv.putText(frame, "Hello", (50,100),
                 cv.FONT_HERSHEY_SIMPLEX,
                 2,              # font_scale
                 (150,0,150),    # color
                 4)              # thickness
 
-    # Operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
     # Display the resulting frame
     cv.imshow('frame', frame)
+
+    # Save prev frame 
+    prev_gray = gray 
+
+
+    # See if user wants to quit
     if cv.waitKey(1) == ord('q'):
         break
  
