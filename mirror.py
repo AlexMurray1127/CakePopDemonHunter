cda25ce368bafeedd2e04561858a3be7a0047931
AlexMurray1127 <alexmurray1127@gmail.com>
2025-11-10 10:40:50 -0500

    Text Feedback
    Added text feedback in the top left corner.

--- mirror.py
+++ mirror.py
@@ -1,2 +1,37 @@
 # This file will be minimal opencv file for mirroring the webcam
-print('This is mirror.py')
+
+import numpy as np
+import cv2 as cv
+ 
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+ 
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    frame = cv.flip(frame, 1)
+
+    # Display Text
+    cv.putText(frame, "Hello", (50,100),
+                cv.FONT_HERSHEY_SIMPLEX,
+                2,              # font_scale
+                (150,0,150),    # color
+                4)              # thickness
+
+    # Operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows() 
+
